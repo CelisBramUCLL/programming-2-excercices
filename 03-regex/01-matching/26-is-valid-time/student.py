8b0affdd099396c04da97260655b1f2478a6512d
Bram Celis <bram.celis1@student.ucll.be>
2024-03-12 18:14:44 +0100

    klaar met 01 matching

--- 03-regex/01-matching/26-is-valid-time/student.py
+++ 03-regex/01-matching/26-is-valid-time/student.py
@@ -3,4 +3,4 @@ import re
 
 
 def is_valid_time(string):
-    return re.fullmatch(r"([0-9]{2}):([0-9]{2}):([0-9]{2})(\.)", string)
+    return re.fullmatch(r"([0-9]{2}):([0-9]{2}):([0-9]{2})(\.[0-9]{3})?", string)
